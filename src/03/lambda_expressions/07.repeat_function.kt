a787f292f5543bd912e542fbbe6837f9ac320855
Martin Wilches <martindotdev@gmail.com>
2024-05-05 20:21:12 -0500

    Function repeat

---
+++ src/03/lambda_expressions/07.repeat_function.kt
@@ -0,0 +1,13 @@
+fun main() {
+    /**
+     * Las funciones que muestran otras funciones o reciben funciones como parametros se denominan funciones de orden superior.
+     * La funcion "repeat()" es una funcion de orden superior que representa una forma concisa de expresar un bucle for
+     *      repeat(times: Int, action: (Int) -> Unit)
+     *
+     * - times es la cantidad de veces que debe ocurrir la accion.
+     * - action es una funcion que toma un solo parametro el cual es la cantidad de veces que se ejecuto la accion hasta el momento.
+     * */
+    repeat(10) {
+        println(it + 1)
+    }
+}
