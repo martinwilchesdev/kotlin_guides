bb26a9254a1daf01b468025144516c11875adba0
Martin Wilches <martindotdev@gmail.com>
2024-08-07 18:31:36 -0500

    Valores genericos en las propiedades de una clase

--- src/05/generics_objects_extensions/01.generics.kt
+++ src/05/generics_objects_extensions/01.generics.kt
@@ -5,10 +5,10 @@ fun main() {
      *          [properties]
      *      }
      *
-     * El tipo de dato que utiliza el tipo generico se pasa como parametro entre `<>` cuando se crea una instancia de la clase.
+     * El tipo de dato que utilizara eventualmente el tipo generico se pasa como parametro entre `<>` cuando se crea una instancia de la clase.
      *      val [instance name] = [class name]<generic data type>([parameters])
      *
-     * El tipo de dato de la propiedad definido durante la instanciacion, debe coincider con el tipo de dato generico proporcionado para la clase
+     * El tipo de dato de la propiedad definido durante la instanciacion, debe coincider con el tipo de dato generico proporcionado para la clase.
      * */
     val q1 = GenericClassName<String>("What is?", "true","medium")
     val q2 = GenericClassName<Boolean>("What is?", true, "easy")
